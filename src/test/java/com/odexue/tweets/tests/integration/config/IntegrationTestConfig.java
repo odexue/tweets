0229344b04e9228b188af5fe353289be7a268da7
odexue <odexue@gmail.com>
2021-08-16 20:51:58 +0100

    trying still to show report

--- src/test/java/com/odexue/tweets/tests/integration/config/IntegrationTestConfig.java
+++ src/test/java/com/odexue/tweets/tests/integration/config/IntegrationTestConfig.java
@@ -1,12 +1,13 @@
 package com.odexue.tweets.tests.integration.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@TestConfiguration
 public class IntegrationTestConfig {
 
     @Bean
